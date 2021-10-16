d0bb7718c8deedf5a0c8de68978b4a99ab071fbb
Musa <a.musamidin@gmail.com>
2021-10-16 13:48:57 +0600

    *rest api todo

---
+++ models/Task.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "task".
+ *
+ * @property int $id
+ * @property string $title
+ * @property int|null $priority
+ * @property int|null $done
+ * @property int|null $version
+ */
+class Task extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'task';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['priority', 'done', 'version'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'priority' => 'Priority',
+            'done' => 'Done',
+            'version' => 'Version',
+        ];
+    }
+}
